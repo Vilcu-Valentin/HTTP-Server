b143e6e518696f38715fd0c1991ce1b46a15f7c1
vally <valentinvilcu@yahoo.com>
2024-01-05 13:52:43 +0200

    Added GET request and response

--- server.c
+++ server.c
@@ -1,34 +1,92 @@
-#include <sys/socket.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 #include <fcntl.h>
 #include <sys/sendfile.h>
-#include <unistd.h>
-#include <netinet/in.h>
 
-void main() {
-    int s = socket(AF_INET, SOCK_STREAM, 0);
-    struct sockaddr_in addr = {
-        AF_INET,
-        0x901f,
-        0
-    };
+#define BUFFER_SIZE 4096
 
-    bind(s, &addr, sizeof(addr));
+void handle_client_request(int client_fd);
 
-    listen(s, 10);
+int main() {
+    int server_fd, client_fd;
+    struct sockaddr_in addr;
+    int addr_len = sizeof(addr);
 
-    int client_fd = accept(s, 0, 0);
+    // Create a socket
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd < 0) {
+        perror("Socket failed");
+        exit(EXIT_FAILURE);
+    }
 
-    char buffer[256] = {0};
-    recv(client_fd, buffer, 256, 0);
+    // Define the server address
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(8080);  // Using port 8080
+    addr.sin_addr.s_addr = INADDR_ANY;
 
-    // GET /file.html .....
+    // Bind the socket to the server address
+    if (bind(server_fd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+        perror("Bind failed");
+        exit(EXIT_FAILURE);
+    }
 
-    char* f = buffer + 5;
-    *strchr(f, ' ') = 0;
-    int opened_fd = open(f, O_RDONLY);
-    sendfile(client_fd, opened_fd, 0, 256);
-    close(opened_fd);
-    close(client_fd);
-    close(s);
+    // Listen for connections
+    if (listen(server_fd, 10) < 0) {
+        perror("Listen failed");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Server is listening on port 8080...\n");
+
+    // Accept client connections and handle them
+    while (1) {
+        client_fd = accept(server_fd, (struct sockaddr *)&addr, (socklen_t *)&addr_len);
+        if (client_fd < 0) {
+            perror("Accept failed");
+            continue;
+        }
+
+        handle_client_request(client_fd);
+        close(client_fd);
+    }
+
+    return 0;
+}
+
+void handle_client_request(int client_fd) {
+    char buffer[BUFFER_SIZE];
+    int bytes_read = recv(client_fd, buffer, BUFFER_SIZE, 0);
+    if (bytes_read < 0) {
+        perror("Error reading request");
+        return;
+    }
+
+    // Parse HTTP method and path
+    char method[8], path[256];
+    sscanf(buffer, "%s %s", method, path);  
+
+    // Only handling GET requests for simplicity
+    if (strcmp(method, "GET") == 0) {
+        // Open and send the requested file
+        int file_fd = open(path + 1, O_RDONLY); // +1 to skip the leading '/'
+        if (file_fd < 0) {
+            // File not found, send 404 Not Found response
+            char *response = "HTTP/1.1 404 Not Found\r\nContent-Length: 0\r\n\r\n";
+            send(client_fd, response, strlen(response), 0);
+        } else {
+            // File found, send 200 OK response followed by file content
+            char *header = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n";
+            send(client_fd, header, strlen(header), 0);
+            sendfile(client_fd, file_fd, NULL, BUFFER_SIZE);
+            close(file_fd);
+        }
+    } else {
+        // Method not implemented, send 501 Not Implemented response
+        char *response = "HTTP/1.1 501 Not Implemented\r\nContent-Length: 0\r\n\r\n";
+        send(client_fd, response, strlen(response), 0);
+    }
 }
